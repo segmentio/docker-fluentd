08c88fedde8b3235450ff66dd5ed552e74642fe5
Christoph Dwertmann <cdwertmann@gmail.com>
2015-07-09 10:29:58 +1000

    move to dedicated fluentd port

--- run.sh
+++ run.sh
@@ -31,11 +31,11 @@ cat > /etc/fluent/fluent.conf <<EOF
 
 <match container.**>
   buffer_type memory
-  flush_interval 10s
   type forward
   heartbeat_type tcp
   <server>
     host $FLUENTD_HOST
+    port 24225
   </server>
 </match>
 
